d2fffb7976db914c38cabcb718c5aa4fa29cfed8
Fede Bertazzo <fedebertazzo@gmail.com>
2019-10-08 21:27:06 -0300

    Update de archivos

--- Prueba_sensores_v2.py
+++ Prueba_sensores_v2.py
@@ -11,23 +11,23 @@ from time import sleep
 sense=SenseHat()
 """ Brillo del SenseHat"""
 sense.low_light = True
-while True:
-#    tTEMPERATURA
-    temp = sense.get_temperature()
-    print 'Temperatura: {0:.2f} °C'.format(temp)
-    sense.show_message("Temp: {0:.2f} C".format(temp), scroll_speed=.1, text_colour=[255,255,255],back_colour=[0,0,0])
-#    PRESIÓN
-    pres= sense.get_pressure()
-    print 'Pres: {0:.3f} mBar'.format(pres)
-    sense.show_message("Pres: {0:.3f} mbar".format(pres))
-#    HUMEDAD
-    hum= sense.get_humidity()
-    print 'Hum: {0:.2f} %'.format(hum)
-    sense.show_message("Hum: {0:.2f} % ".format(hum))
-    print '----------------------- \n'
-    sleep(1)
-    sense.show_letter("?")
-    jmove = sense.stick.wait_for_event()
+#while True:
+##    tTEMPERATURA
+#    temp = sense.get_temperature()
+#    print 'Temperatura: {0:.2f} °C'.format(temp)
+#    sense.show_message("Temp: {0:.2f} C".format(temp), scroll_speed=.1, text_colour=[255,255,255],back_colour=[0,0,0])
+##    PRESIÓN
+#    pres= sense.get_pressure()
+#    print 'Pres: {0:.3f} mBar'.format(pres)
+#    sense.show_message("Pres: {0:.3f} mbar".format(pres))
+##    HUMEDAD
+#    hum= sense.get_humidity()
+#    print 'Hum: {0:.2f} %'.format(hum)
+#    sense.show_message("Hum: {0:.2f} % ".format(hum))
+#    print '----------------------- \n'
+#    sleep(1)
+#    sense.show_letter("?")
+#    jmove = sense.stick.wait_for_event()
 #    print 'Joystick {}'.format(jmove.direction)
     
 #    mk1= 0
@@ -36,10 +36,21 @@ while True:
 #    if jmove =down:
 #        sensores(mk1)
 #        sense.show_message("{0}: {1:.2f} {2}".format(sensores(mk1), 
-#while True:
-#    jmove = sense.stick.wait_for_event(emptybuffer=True)
-#    jmove = sense.stick.wait_for_event()
-#    if jmove.direction == "up":
+    
+    
+def sensor():
+    sense.clear()
+    pres=sense.get_pressure()
+    temp=sense.get_temperature()
+    hum=sense.get_humidity()
+    
+    
+while True:
+    event = sense.stick.get_event()
+    if event.action in ('pressed'):
+        event = sensor({'up'=pres,'down'=hum,'left'=temp,'right'=temp1}.get(event.direction))
+        
+#        
 #        sense.show_letter("P")
 #        sleep(1)
 #        pres = sense.get_pressure()
@@ -73,7 +84,7 @@ while True:
 #        jmove = sense.stick.wait_for_event(emptybuffer=True)
 #        
 #            
-            
-            
-
-        
+#            
+#            
+#
+#        
